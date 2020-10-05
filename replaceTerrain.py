d8798ce1e608ff90df3641542620749477a39abb
dongresource <dongresource@protonmail.com>
2020-10-05 18:21:10 +0200

    Cleaned up terrain editing scripts.
    Reverted to the RGB approach for now.

--- replaceTerrain.py
+++ replaceTerrain.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python3
 
+import sys
+import struct
 from unitypack.asset import Asset
 from PIL import Image
-import struct
 
 MAX_HEIGHT = 20000
-REPLACE_OFFSET = 0x70335c + 4
 
-img = Image.open('/tmp/DongResources_12_09.png')
-print('loaded image.')
+def main(imgpath, offset, output):
+    offset += 4
 
-pixels = img.tobytes()
+    img = Image.open(imgpath)
+    print('loaded image.')
 
-#int16s = []
-buf = bytes()
-for i in range(len(pixels)):
-    p = pixels[i]
-    buf += struct.pack('<H', p)
+    pixels = img.tobytes()
 
-#print('length is', len(int16s))
+    buf = bytes()
+    for i in range(0, len(pixels), 3):
+        p = pixels[i]
+        buf += struct.pack('<H', int((p * MAX_HEIGHT)/255))
 
-#buf = b''
-#for height in int16s:
-#    buf += (struct.pack('<h', height))
+    print('buffer length is', len(buf))
+    with open(output, 'r+b') as f:
+        f.seek(offset)
+        f.write(buf)
 
-#buf = bytes(buf)
-print('pixels:', len(pixels))
-print('length is', len(buf))
-with open('/tmp/DongResources_12_09.resourceFile_unpacked/patched_CustomAssetBundle-51ff75b52842e40e9b4c0dd6da9d91f1', 'r+b') as f:
-    f.seek(REPLACE_OFFSET)
-    f.write(buf)
-print('done.')
+    print('done.')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('''usage: replaceTerrain.py input.png offset OutputAssetBundle
+
+NOTE: OutputAssetBundle will be modified in-place, starting from the supplied offset.
+It will not check if the offset is correct nor will it make a backup of the asset bundle first.''', file=sys.stderr)
+        sys.exit(1)
+
+    main(sys.argv[1], int(sys.argv[2], 0), sys.argv[3])
